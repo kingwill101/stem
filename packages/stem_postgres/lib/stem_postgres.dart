7f07fbd486167b84174348d64056759c640496e0
kingwill101 <hey@glenfordwilliams.com>
2025-12-31 12:10:43 -0500

    Remove legacy Postgres migration helper

--- packages/stem_postgres/lib/stem_postgres.dart
+++ packages/stem_postgres/lib/stem_postgres.dart
@@ -3,7 +3,6 @@ export 'src/brokers/postgres_broker.dart' show PostgresBroker;
 export 'src/control/postgres_revoke_store.dart' show PostgresRevokeStore;
 export 'src/postgres/postgres_client.dart'
     show PostgresClient, PostgresResult, PostgresRow, PostgresSession;
-export 'src/postgres/postgres_migrations.dart' show PostgresMigrations;
 export 'src/scheduler/postgres_lock_store.dart' show PostgresLockStore;
 export 'src/scheduler/postgres_schedule_store.dart' show PostgresScheduleStore;
 export 'src/workflow/postgres_factories.dart'
